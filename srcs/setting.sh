db64b30bf2f62a423a517e96815d4dfff038c7ba
JaeminKang <kangjm2@hanyang.ac.kr>
2021-02-16 20:39:51 +0900

    almost finish

--- srcs/setting.sh
+++ srcs/setting.sh
@@ -2,7 +2,7 @@
 
 service mysql start
 echo "CREATE DATABASE IF NOT EXISTS moodle;
-ALTER DATABASE moodle DEFAULT CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE moodle DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER 'user'@'%' identified by 'password';
 GRANT ALL PRIVILEGES ON moodle.* TO 'user'@'%' IDENTIFIED BY 'password';
 FLUSH PRIVILEGES;
